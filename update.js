22a56be6899eff09c03ea953f02228d46cc4f2ca
uxsef <88479036+uxsef@users.noreply.github.com>
2024-09-11 07:22:17 +0300

    Add files via upload

---
+++ update.js
@@ -0,0 +1,12 @@
+var calendario = {
+    current: '3',
+    previous: '2',
+    update: '1',
+    download: 'https://raw.githubusercontent.com/codrops/Calendario/master/js/jquery.calendario.js',
+    msg: 'CALENDARIO MSG' +
+         '\n===============' +
+         '\nNEW VERSION (3.2.0) AVAILABLE!' +
+         '\nDOWNLOAD : https://raw.githubusercontent.com/codrops/Calendario/master/js/jquery.calendario.js' +
+         '\nMINIFIED : https://raw.githubusercontent.com/codrops/Calendario/master/js/jquery.calendario.min.js' +
+         '\n=================================================================================================='
+};
